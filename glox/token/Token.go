cc7ba83d0952a52d620e532c7b7633f1f265c2e5
madraceee <raajanitheesh@gmail.com>
2025-01-28 23:29:56 +0530

    added bool type to literal object

--- glox/token/Token.go
+++ glox/token/Token.go
@@ -8,19 +8,27 @@ const (
 	NOT_ASSIGNED_TYPE ObjectType = iota
 	NUMBER_TYPE
 	STRING_TYPE
+	BOOL_TYPE
 )
 
 type Object struct {
 	ObjType     ObjectType
 	Value_str   string
 	Value_float float64
+	Value_bool  bool
 }
 
-func (o *Object) GetStringValue() string {
+func GetStringValue(o Object) string {
 	if o.ObjType == NUMBER_TYPE {
 		return strconv.FormatFloat(o.Value_float, 'f', -1, 64)
 	} else if o.ObjType == STRING_TYPE {
 		return o.Value_str
+	} else if o.ObjType == BOOL_TYPE {
+		if o.Value_bool {
+			return "true"
+		} else {
+			return "false"
+		}
 	}
 	return ""
 }
@@ -42,5 +50,5 @@ func NewToken(tokenType TokenType, lexeme string, literal Object, line int) Toke
 }
 
 func (t Token) String() string {
-	return GetTokenType(t.TokenType) + " " + t.Lexeme + " " + t.Literal.GetStringValue()
+	return GetTokenType(t.TokenType) + " " + t.Lexeme + " " + GetStringValue(t.Literal)
 }
