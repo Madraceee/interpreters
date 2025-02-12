17d42d94978b67bbaa41b822cdf271f9854611f3
madraceee <raajanitheesh@gmail.com>
2025-02-12 22:57:45 +0530

    added logical expressions

--- clox/vm.c
+++ clox/vm.c
@@ -33,6 +33,10 @@ static void runtimeError(const char* format,...){
 
 }
 
+static bool isFalsey(Value value) {
+	return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 void initVM(){
 	resetStack();
 }
@@ -86,6 +90,9 @@ static InterpretResult run(){
 				printf("\n");
 				return INTERPRET_OK;
 			}
+			case OP_NOT: 
+				push(BOOL_VAL(isFalsey(pop())));
+				break;
 			case OP_NEGATE: {
 				if (!IS_NUMBER(peek(0))) {
 					runtimeError("Operand must be a number.");
@@ -102,6 +109,14 @@ static InterpretResult run(){
 			case OP_NIL: push(NIL_VAL); break;
 			case OP_TRUE: push(BOOL_VAL(true)); break;
 			case OP_FALSE: push(BOOL_VAL(false)); break;
+			case OP_EQUAL: {
+				Value b = pop();
+				Value a = pop();
+				push(BOOL_VAL(valuesEqual(a,b)));
+				break;
+			}
+			case OP_GREATER: BINARY_OP(BOOL_VAL, >); break;
+			case OP_LESS: BINARY_OP(BOOL_VAL, <); break;
 			case OP_ADD: BINARY_OP(NUMBER_VAL,+); break;
 			case OP_SUBTRACT: BINARY_OP(NUMBER_VAL,-); break;
 			case OP_MULTIPLY: BINARY_OP(NUMBER_VAL,*); break;
