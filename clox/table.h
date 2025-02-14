82cb5cedaf2d351fcf809ed3d23dc133c05d8ed3
madraceee <raajanitheesh@gmail.com>
2025-02-14 21:16:38 +0530

    added hash table

---
+++ clox/table.h
@@ -0,0 +1,27 @@
+#ifndef clox_table_h
+#define clox_table_h
+
+#include "common.h"
+#include "value.h"
+
+// Added table for variable lookup
+// A closed hashing table which uses hash from Obj and stores the Value
+typedef struct{
+	ObjString* key;
+	Value value;
+} Entry;
+
+typedef struct {
+	int count;
+	int capacity;
+	Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableSet(Table* table, ObjString* key, Value value);
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(Table* from, Table* to);
+
+#endif
